8b517457a1894e32c13e3d6aa5ce283ec90b8acc
Nauman-Mirza <nmirza403@gmail.com>
2023-09-06 10:30:26 +0500

    First Commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->unsignedInteger('age'); // Add the 'age' column as an unsigned integer
+            $table->date('date_of_birth'); // Add the 'date_of_birth' column as a date
+            $table->string('number'); // Add the 'number' column as a string
+            $table->text('address'); // Add the 'address' column as text
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
